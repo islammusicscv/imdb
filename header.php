ecb3a9d7ae9a94326382adb89ffe2faec817d600
islammusicscv <islam.music@scv.si>
2021-04-09 09:14:33 +0200

    dodana registracija

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
         <div class="row flex-nowrap justify-content-between align-items-center">
           <div class="col-4 pt-1">
             <!--<a class="text-muted" href="#">Subscribe</a>-->
+            <img src="./assets/img/logo.png" alt="logo" width="80px" />
           </div>
           <div class="col-4 text-center">
             <a class="blog-header-logo text-dark" href="#">Large</a>

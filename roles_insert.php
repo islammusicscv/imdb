25c9c289991a575874510ff263fa75b9cebabe2d
islammusicscv <islam.music@scv.si>
2021-05-14 10:39:32 +0200

    igralci vloge - popravek

--- roles_insert.php
+++ roles_insert.php
@@ -4,18 +4,19 @@
 
     $movie_id = (int) $_POST['movie_id'];
     $actors = $_POST['actors'];
+    $role = $_POST['role'];
 
     //vse vloge, najprej izbrišem
     $query = "DELETE FROM roles WHERE movie_id=?";
     $stmt = $pdo->prepare($query);
-    $stmt->execute([$move_id]);
+    $stmt->execute([$movie_id]);
 
     //vnesi vse vloge
     foreach ($actors as $actor_id) {
-        $role = $_POST["role[$actor_id]"];
+        //$role_value = ;
         $query = "INSERT INTO roles(actor_id,movie_id,role) VALUES (?,?,?)";
         $stmt = $pdo->prepare($query);
-        $stmt->execute([$actor_id,$movie_id,$role]);
+        $stmt->execute([$actor_id,$movie_id,$role[$actor_id]]);
     }
 
     header("Location: movie.php?id=$movie_id"); die();
